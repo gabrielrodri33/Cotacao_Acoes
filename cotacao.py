eea37fa169d352b54bff7492b7084233f7f969b4
gabrielrodri33 <gabrielrodri333@gmail.com>
2025-07-17 20:50:05 -0300

    Criado função para pegar os números de telefone do .env e criado função para cotar ações

--- cotacao.py
+++ cotacao.py
@@ -1,11 +1,37 @@
 from dotenv import load_dotenv
+from io import StringIO
 import os
+import requests
+import pandas as pd
+import csv
+import json
+import yfinance as yf
 
 def env():
     load_dotenv(override=True)
 
-    api_key = os.getenv("API_KEY")
+    phone = list(map(int, os.getenv('NUMEROS').split(',')))
 
-    numeros = list(map(int, os.getenv('NUMEROS').split(',')))
+    return phone
 
-    return api_key, numeros
+def cotacao_acao():
+    acoes = ["MXRF11.SA", "BBAS3.SA", "TAEE4.SA"]
+
+    for acao in acoes:
+        dado = yf.Ticker(acao)
+        info = dado.info
+
+        print(json.dumps(info, indent=4, ensure_ascii=False))
+
+        print(f"\n📈 {acao}")
+        print(f"Preço atual: R$ {info.get('currentPrice')}")
+        print(f"Abertura: R${info.get('open')}")
+        print(f"Prévia de fechamento: R${info.get('previousClose')}")
+        print(f"Máxima diária: R$ {info.get('dayHigh')}")
+        print(f"Mínima diária: R$ {info.get('dayLow')}")
+
+def main():
+    cotacao_acao()
+
+if __name__ == "__main__":
+    main()
