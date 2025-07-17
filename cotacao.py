6dba4d17f020f99eff0399019327c96b590bb748
gabrielrodri33 <gabrielrodri333@gmail.com>
2025-07-16 21:17:17 -0300

    Criado as variáveis de ambiente e adicionado no código

--- cotacao.py
+++ cotacao.py
@@ -1,1 +1,11 @@
-print("Hello World!")
+from dotenv import load_dotenv
+import os
+
+def env():
+    load_dotenv(override=True)
+
+    api_key = os.getenv("API_KEY")
+
+    numeros = list(map(int, os.getenv('NUMEROS').split(',')))
+
+    return api_key, numeros
